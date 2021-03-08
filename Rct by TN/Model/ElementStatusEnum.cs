393ebdb537d0440d22d2c6d1c440588ed8a2ca41
Eszter B <xqw70y@inf.elte.hu>
2021-03-08 16:24:12 +0100

    ElementStatus and GuestStatus added.

---
+++ Rct by TN/Model/ElementStatusEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rct_by_TN.Model
+{
+    class ElementStatusEnum
+    {
+        public enum ElementStatus
+        {
+            Operate,
+            InWaiting,
+            InBuild
+        }
+    }
+}
---
+++ Rct by TN/Model/GuestStatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rct_by_TN.Model
+{
+    class GuestStatusEnum
+    {
+        public enum GuestStatus
+        {
+            Searching,
+            Waiting,
+            Busy,
+            Aimless
+        }
+    }
+}
