f4846c259f4432e69e91f4c346d49cd41da9b638
Eszter B <xqw70y@inf.elte.hu>
2021-03-08 17:31:49 +0100

    enum files fixed

---
+++ Rct by TN/Model/ElementStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rct_by_TN.Model
+{
+        public enum ElementStatus
+        {
+            Operate,
+            InWaiting,
+            InBuild
+        }
+}
---
+++ Rct by TN/Model/GuestStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rct_by_TN.Model
+{
+        public enum GuestStatus
+        {
+            Searching,
+            Waiting,
+            Busy,
+            Aimless
+        }
+}
