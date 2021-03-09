a4ea7294a0ce7a462f5ed69d0e717e1e36901fc9
Li Dominik <ef9xhw@inf.elte.hu>
2021-03-09 15:14:44 +0100

    Rename solution file and root namespace

---
+++ RctByTN/Model/ElementStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RctByTN.Model
+{
+    public enum ElementStatus
+    {
+        Operate,
+        InWaiting,
+        InBuild
+    }
+}
---
+++ RctByTN/Model/GuestStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RctByTN.Model
+{
+        public enum GuestStatus
+        {
+            Searching,
+            Waiting,
+            Busy,
+            Aimless
+        }
+}
