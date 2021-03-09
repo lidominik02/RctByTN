e7fcd158ee2f6f390f18bf4889110d78faf7e29e
Li Dominik <ef9xhw@inf.elte.hu>
2021-03-09 19:28:15 +0100

    Add datamembers to RctModel class. Add Guest and ParkElement class

--- RctByTN/Model/RctModel.cs
+++ RctByTN/Model/RctModel.cs
@@ -6,5 +6,24 @@ namespace RctByTN.Model
 {
     class RctModel
     {
+        bool isParkOpen;
+        Int32 cash;
+        Int32 income;
+        Int32 outcome;
+        List<Guest> guestList;
+        List<ParkElement> parkElementList;
+
+        public bool IsParkOpen { get => isParkOpen; set => isParkOpen = value; }
+        public int Cash { get => cash; set => cash = value; }
+        public int Income { get => income; set => income = value; }
+        public int Outcome { get => outcome; set => outcome = value; }
+        internal List<Guest> GuestList { get => guestList; set => guestList = value; }
+        internal List<ParkElement> ParkElementList { get => parkElementList; set => parkElementList = value; }
+
+        public RctModel()
+        {
+            GuestList = new List<Guest>();
+            ParkElementList = new List<ParkElement>();
+        }
     }
 }
