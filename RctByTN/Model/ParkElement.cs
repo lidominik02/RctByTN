51462486c77ba9200c1f57fe4b8e7548d1a8d959
Li Dominik <ef9xhw@inf.elte.hu>
2021-03-10 16:42:48 +0100

    Add datamembers and ModifyGuest method to ParkElement class.

--- RctByTN/Model/ParkElement.cs
+++ RctByTN/Model/ParkElement.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace RctByTN.Model
 {
-    class ParkElement
+    abstract class ParkElement
     {
+        protected Int32 x;
+        protected Int32 y;
+        protected Int32 buildCost;
+        protected ElementStatus status;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int BuildCost { get => buildCost; set => buildCost = value; }
+        public ElementStatus Status { get => status; set => status = value; }
+
+        public ParkElement(Int32 x, Int32 y, Int32 buildcost)
+        {
+            this.X = x;
+            this.Y = y;
+            this.BuildCost = buildcost;
+        }
+
+        public abstract void ModifyGuest(Guest guest);
     }
 }
