205adb7d2284a6a498616d80c470870ed695b429
Li Dominik <ef9xhw@inf.elte.hu>
2021-03-10 16:44:48 +0100

    Initialize isParkOpen, income and outcome datamembers in contructor

--- RctByTN/Model/RctModel.cs
+++ RctByTN/Model/RctModel.cs
@@ -6,24 +6,28 @@ namespace RctByTN.Model
 {
     class RctModel
     {
-        bool isParkOpen;
-        Int32 cash;
-        Int32 income;
-        Int32 outcome;
-        List<Guest> guestList;
-        List<ParkElement> parkElementList;
+        private bool isParkOpen;
+        private Int32 cash;
+        private Int32 income;
+        private Int32 outcome;
+        private List<Guest> guestList;
+        private List<ParkElement> parkElementList;
 
         public bool IsParkOpen { get => isParkOpen; set => isParkOpen = value; }
         public int Cash { get => cash; set => cash = value; }
         public int Income { get => income; set => income = value; }
         public int Outcome { get => outcome; set => outcome = value; }
-        internal List<Guest> GuestList { get => guestList; set => guestList = value; }
-        internal List<ParkElement> ParkElementList { get => parkElementList; set => parkElementList = value; }
+        public List<Guest> GuestList { get => guestList; set => guestList = value; }
+        public List<ParkElement> ParkElementList { get => parkElementList; set => parkElementList = value; }
 
         public RctModel()
         {
             GuestList = new List<Guest>();
             ParkElementList = new List<ParkElement>();
+            IsParkOpen = false;
+            income = outcome = 0;
+            //TODO: to find out the amount of starting cash
+            //cash = ???
         }
     }
 }
