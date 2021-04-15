394bce5082dac070cae6c7c45b220c57fca2916c
Li Dominik <ef9xhw@inf.elte.hu>
2021-04-15 17:29:06 +0200

    Add unit test

---
+++ RctByTN.Test/RctModelTest.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RctByTN.Model;
+
+namespace RctByTN.Test
+{
+    [TestClass]
+    public class RctModelTest
+    {
+        private RctModel _model;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _model = new RctModel();
+        }
+
+        [TestMethod]
+        public void RctModelConstructorTest()
+        {
+            Assert.AreEqual(_model.IsParkOpen,false);
+            Assert.AreEqual(_model.Income, 0);
+            Assert.AreEqual(_model.Outcome, 0);
+            Assert.AreEqual(_model.Cash, 1000);
+            Assert.AreEqual(_model.GameTime, 0);
+            Assert.AreEqual(_model.GuestList.Count, 0);
+            Assert.AreEqual(_model.ParkElementList.Count, 0);
+        }
+    }
+}
