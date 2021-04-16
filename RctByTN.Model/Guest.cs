9f79ad9ade3aa4b1bd761770d1f0b000a4fcf73b
Li Dominik <ef9xhw@inf.elte.hu>
2021-04-16 21:39:43 +0200

    Add property validation to Guest class

--- RctByTN.Model/Guest.cs
+++ RctByTN.Model/Guest.cs
@@ -12,21 +12,24 @@ namespace RctByTN.Model
         private int mood;
         private int money;
         private int intolerance;
-        private GuestStatus status;
         private bool hasCoupon;
-        private (int,int) destination;
+        private GuestStatus status;
+        private (int, int) destination;
+        private (int, int) prevCoords;
+        private (int, int) lastCrossRoad;
 
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Money { get; set; }
-        public int Hunger { get; set; }
-        public int Mood { get; set; }
-        public GuestStatus Status { get; set; }
-        public bool HasCoupon { get; set; }
-        public (int,int) Destination { get; set; }
-        public (int, int) PrevCoords { get; set; }
-        public (int,int) LastCrossRoad { get; set; }
-        public int Intolerance { get => intolerance; set => intolerance = value; }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Hunger { get => hunger; set { hunger = value > 0 ? (value > 10 ? 10 : value) : 0; } }
+        public int Mood { get => mood; set { mood = value > 0 ? (value > 10 ? 10 : value) : 0; } }
+        public int Money { get => money; set { money = value > 0 ? value : 0; } }
+        public int Intolerance { get => intolerance; set { intolerance = value > 0 ? value : 0; } }
+        public GuestStatus Status { get => status; set => status = value; }
+        public bool HasCoupon { get => hasCoupon; set => hasCoupon = value; }
+        public (int, int) Destination { get => destination; set => destination = value; }
+        public (int, int) LastCrossRoad { get => lastCrossRoad; set => lastCrossRoad = value; }
+        public (int, int) PrevCoords { get => prevCoords; set => prevCoords = value; }
+        
 
         public Guest(int x, int y, bool hasCoupon)
         {
